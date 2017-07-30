788fad8c68449f12b4730c550e1b719bc2411544
Keith Offer <keithoffer@gmail.com>
2017-07-30 20:20:42 +1000

    FEATURE: Allow for file name, size and path to be saved along with DICOM tags

--- QDICOMMiner.py
+++ QDICOMMiner.py
@@ -20,6 +20,7 @@ import os
 import sys
 import re
 import json
+from enum import Enum
 # pydicom is MIT licenced
 try:
     import dicom as pydicom
@@ -29,7 +30,7 @@ import pydicom._dicom_dict as dicom_dict  # TODO: Is this a good idea? Should I 
 # PyQt is GPL v3 licenced
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog, QMessageBox, QPushButton, QCompleter, QLineEdit, QHBoxLayout, \
-    QLabel, QAbstractItemView, QListWidgetItem
+    QLabel, QAbstractItemView, QListWidgetItem, QComboBox
 from PyQt5.QtCore import QSettings, Qt, QThread, QStringListModel, QObject, pyqtSignal, QUrl
 from PyQt5.QtGui import QDesktopServices
 # Files from this project
@@ -40,6 +41,14 @@ from ui.mainWindow import Ui_MainWindow
 dicom_tag_regex = r'\(((?i)[\da-f]{4}),((?i)[\da-f]{4})\)'
 __version__ = '1.0.1'
 
+class AttributeOptions(Enum):
+    DICOM_TAG = 'Dicom Tag'
+    FILE_INFORMATION = 'File Information'
+
+class FileOptions(Enum):
+    FILE_SIZE = 'File Size (MB)'
+    FILE_NAME = 'File Name'
+    FILE_PATH = 'File Path'
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -127,12 +136,16 @@ class MainWindow(QtWidgets.QMainWindow):
         QDesktopServices.openUrl(QUrl.fromLocalFile(folder_location))
 
     def save_template(self):
-        dic = {'DICOM_tag': []}
+        dic = {'DICOM_tag': [], 'File_information': []}
 
         for index in range(self.ui.listWidget.count()):
             custom_widget = self.ui.listWidget.itemWidget(self.ui.listWidget.item(index))
-            text = custom_widget.lineEdit.text()
-            dic['DICOM_tag'].append(text)
+            if custom_widget.comboBoxAttributeChoice.currentText() == AttributeOptions.DICOM_TAG.value:
+                text = custom_widget.lineEdit.text()
+                dic['DICOM_tag'].append(text)
+            else:
+                text = custom_widget.comboBoxFileOption.currentText()
+                dic['File_information'].append(text)
 
         filepath = QFileDialog.getSaveFileName(self, 'Save template file', '.', '(*.json)')[0]
         if filepath != '':
@@ -158,6 +171,8 @@ class MainWindow(QtWidgets.QMainWindow):
                 try:
                     for tag in dic['DICOM_tag']:
                         self.add_new_list_widget(default_text=tag)
+                    for file_option in dic['File_information']:
+                        self.add_new_list_widget(attribute_type=AttributeOptions.FILE_INFORMATION,file_information=file_option)
                 except KeyError:
                     msg_box = QMessageBox()
                     msg_box.setWindowTitle("Error")
@@ -183,14 +198,27 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.progressBar.setMaximum(num)
 
     # The checked variable is emitted from the signal, but we don't use it here
-    def add_new_list_widget(self, checked=False, default_text=''):
+    def add_new_list_widget(self, checked=False, default_text='',attribute_type=AttributeOptions.DICOM_TAG,file_information=None):
         new_list_widget_item = QListWidgetItem()
         custom_widget = CustomListWidget()
         new_list_widget_item.setSizeHint(custom_widget.sizeHint())
         custom_widget.lineEdit.setCompleter(self.completer)
         custom_widget.lineEdit.textChanged.connect(self.line_edit_text_changed)
-        custom_widget.lineEdit.setText(default_text)
         custom_widget.pushButton.clicked.connect(lambda: self.remove_widget_from_list(new_list_widget_item))
+        if attribute_type == AttributeOptions.DICOM_TAG:
+            custom_widget.lineEdit.setText(default_text)
+        else:
+            # TODO: Should I throw an exception / error message if the index isn't >= 0?
+            file_info_index = custom_widget.comboBoxFileOption.findText(file_information)
+            if file_info_index >= 0:
+                custom_widget.comboBoxFileOption.setCurrentIndex(file_info_index)
+                custom_widget.comboBoxFileOption.setVisible(True)
+                custom_widget.lineEdit.setVisible(False)
+
+        attribute_index = custom_widget.comboBoxAttributeChoice.findText(attribute_type.value)
+        if attribute_index >= 0:
+            custom_widget.comboBoxAttributeChoice.setCurrentIndex(attribute_index)
+
         self.ui.listWidget.addItem(new_list_widget_item)
         self.ui.listWidget.setItemWidget(new_list_widget_item, custom_widget)
 
@@ -238,40 +266,49 @@ class MainWindow(QtWidgets.QMainWindow):
             msg_box.exec()
             if msg_box.clickedButton() != overwrite_button:
                 return
-        header = ''
-        tags = []
+        header_DICOM = ''
+        header_file_info = ''
+        dicom_tags = []
+        file_attributes = []
         for index in range(self.ui.listWidget.count()):
             custom_widget = self.ui.listWidget.itemWidget(self.ui.listWidget.item(index))
-            text = custom_widget.lineEdit.text()
-            try:
-                if text == '':
-                    raise KeyError
-                if re.match(dicom_tag_regex, text):
-                    search_results = re.search(dicom_tag_regex, text)
-                    # Note that group 0 is the whole match that we don't want
-                    tags.append((search_results.group(1), search_results.group(2)))
-                else:
-                    tags.append(self.DICOM_dic[custom_widget.lineEdit.text()])
-            except KeyError:
-                msg_box = QMessageBox()
-                msg_box.setWindowTitle("Error")
-                msg_box.setText('"' + text + '" is not a valid attribute')
-                msg_box.setIcon(QMessageBox.Critical)
-                msg_box.exec()
-                return
-            header += text.replace(',', ' ') + ','
-        header = header[0:-1]  # Remove the last comma
+            # Handle DICOM tags
+            if custom_widget.comboBoxAttributeChoice.currentText() == AttributeOptions.FILE_INFORMATION.value:
+                header_file_info += custom_widget.comboBoxFileOption.currentText() + ','
+                file_attributes.append(custom_widget.comboBoxFileOption.currentText())
+            else:
+                text = custom_widget.lineEdit.text()
+                try:
+                    if text == '':
+                        raise KeyError
+                    if re.match(dicom_tag_regex, text):
+                        search_results = re.search(dicom_tag_regex, text)
+                        # Note that group 0 is the whole match that we don't want
+                        dicom_tags.append((search_results.group(1), search_results.group(2)))
+                    else:
+                        dicom_tags.append(self.DICOM_dic[custom_widget.lineEdit.text()])
+                except KeyError:
+                    msg_box = QMessageBox()
+                    msg_box.setWindowTitle("Error")
+                    msg_box.setText('"' + text + '" is not a valid attribute')
+                    msg_box.setIcon(QMessageBox.Critical)
+                    msg_box.exec()
+                    return
+                header_DICOM += text.replace(',', ' ') + ','
+            # Handle file attributes
+
+        header = (header_file_info + header_DICOM)[0:-1]  # Remove the last comma
 
         self.ui.progressBar.show()
         self.analyse_and_output_data_thread.current_file.connect(lambda num: self.ui.progressBar.setValue(num))
         self.create_csv.connect(self.analyse_and_output_data_thread.run)
         self.analyse_and_output_data_thread.finished.connect(self.csv_making_finished)
-        self.create_csv.emit(self.ui.labelOutputFile.text(), self.ui.labelFolderToAnalysePath.text(), header, tags)
+        self.create_csv.emit(self.ui.labelOutputFile.text(), self.ui.labelFolderToAnalysePath.text(), header, dicom_tags,file_attributes)
 
     def csv_making_finished(self):
         self.ui.progressBar.hide()
 
-    create_csv = pyqtSignal(str, str, str, list)
+    create_csv = pyqtSignal(str, str, str, list,list)
     count_file_number = pyqtSignal(str)
 
 
@@ -283,22 +320,39 @@ class AnalyseAndOutputDataThread(QObject):
         self.moveToThread(self.worker_thread)
         self.worker_thread.start()
 
-    def run(self, output_file, folder_to_analyse, header, tags):
+    def run(self, output_file, folder_to_analyse, header, dicom_tags, file_attributes):
         with open(output_file, 'w') as f:
             f.write(header + '\n')
 
             count = 0
             for dirpath, _, filenames in os.walk(folder_to_analyse):
                 for filename in filenames:
+                    output_line = ''
                     full_path = os.path.join(dirpath, filename)
                     try:
                         ds = pydicom.read_file(full_path)
-                        output_line = ''
-                        for tag in tags:
+                    except pydicom.errors.InvalidDicomError:
+                        continue # If it isn't a valid DICOM file, we'll just skip over it
+
+                    # List the file attributes
+                    for attribute in file_attributes:
+                        try:
+                            if attribute == FileOptions.FILE_NAME.value:
+                                output_line += filename + ','
+                            elif attribute == FileOptions.FILE_PATH.value:
+                                output_line += full_path + ','
+                            elif attribute == FileOptions.FILE_SIZE.value:
+                                output_line += str(round(os.path.getsize(full_path)/(1000*1000),3)) + ','
+                        except (FileNotFoundError, OSError, PermissionError):
+                            pass
+                    # Get the data from the tags
+                    try:
+                        ds = pydicom.read_file(full_path)
+                        for tag in dicom_tags:
                             output_line += get_dicom_value_from_tag(ds, tag) + ','
                         output_line = output_line[0:-1]  # Remove the last comma
                         f.write(output_line + '\n')
-                    except (pydicom.errors.InvalidDicomError, FileNotFoundError, OSError, PermissionError):
+                    except (FileNotFoundError, OSError, PermissionError):
                         pass
                     except OSError as e:
                         if e.args[0] != 6:  # No such device or address
@@ -330,19 +384,34 @@ class CountFilesThread(QObject):
 
     num_of_files = pyqtSignal(int)
 
-
 class CustomListWidget(QtWidgets.QWidget):
     def __init__(self):
         super(CustomListWidget, self).__init__()
 
         self.layout = QHBoxLayout()
         self.lineEdit = QLineEdit()
+        self.comboBoxAttributeChoice = QComboBox()
+        self.comboBoxFileOption = QComboBox()
+        self.comboBoxFileOption.addItems([option.value for option in FileOptions])
+        self.comboBoxFileOption.setVisible(False)
+        self.comboBoxAttributeChoice.addItems([attribute.value for attribute in AttributeOptions])
+        self.comboBoxAttributeChoice.activated[str].connect(self.change_item_type)
         self.lineEdit.setStyleSheet("QLineEdit { background: rgb(255, 0, 0); }")
         self.pushButton = QPushButton("Delete")
-        self.layout.addWidget(self.pushButton)
+        self.layout.addWidget(self.comboBoxAttributeChoice)
+        self.layout.addWidget(self.comboBoxFileOption)
         self.layout.addWidget(self.lineEdit)
+        self.layout.addWidget(self.pushButton)
         self.setLayout(self.layout)
 
+    def change_item_type(self,selection):
+        if selection == AttributeOptions.DICOM_TAG.value:
+            self.comboBoxFileOption.setVisible(False)
+            self.lineEdit.setVisible(True)
+        else:
+            self.comboBoxFileOption.setVisible(True)
+            self.lineEdit.setVisible(False)
+            self.lineEdit.setText('')
 
 class ClickableQLabel(QLabel):
     def __init__(self, parent=None):
